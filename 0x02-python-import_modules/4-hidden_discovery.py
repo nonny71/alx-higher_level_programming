36309823cf0a1e02f37038717f1edc75ef91bbdd
nonny71 <michealnonny71@gmail.com>
2022-12-07 14:54:43 -0800

    python

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-if __name__=='__main__':
-    import sys
+if __name__ == "__main__":
     import hidden_4
-    for n in dir(hidden_4):
-        if n[:2] != "__":
-            print(n)
+    list = dir(hidden_4)
+    for s in list:
+        if s[0] != '_':
+            print("{}".format(s))
