a271d8eb5405d08a532c6347b153ea1c9fc1e060
nonny71 <michealnonny71@gmail.com>
2022-12-07 15:06:31 -0800

    pyhton

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    i = 1
-    sum = 0
-    length = len(sys.argv) - 1
-    while i <= length:
-        sum += int(sys.argv[i])
-        i += 1
-        print("{}".format(sum))
+    from sys import argv
+    len_of_args = len(argv)
+     sum = 0
+     for i in range(1, len_of_args):
+         sum += int(argv[i])
+         print(sum)
