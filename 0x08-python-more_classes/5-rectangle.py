0b0ff64ac5f17cedc9478fa0d0c7c76d418d8b62
nonny71 <michealnonny71@gmail.com>
2023-01-10 01:16:47 -0800

    more classes and objects

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+class Rectangle:
+    """ This class will define a Rectangle """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return (self.width * self.height)
+
+    def perimeter(self):
+        if self.height == 0 or self.width == 0:
+            return 0
+        perimeter = ((self.width + self.height) * 2)
+        return (perimeter)
+
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
+        rectangle = ""
+        width = 0
+        width = "#" * self.width
+        rectangle = width
+        for x in range(self.height - 1):
+            rectangle += "\n" + width
+        return rectangle
+
+    def __repr__(self):
+        return ("Rectangle({:d}, {:d})".format(self.width, self.height))
+
+    def __del__(self):
+        del self
+        print("Bye rectangle...")
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,12 @@
+# 0x08. Python - More Classes and Objects
+
+## 0. Simple rectangle
+Write an empty class Rectangle that defines a rectangle:
+
+## 1. Real definition of a rectangle 
+Write a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)
+
+## 2. Area and Perimeter
+Write a class Rectangle that defines a rectangle by: (based on 1-rectangle.py)
+
+## 3. String representation
